bc271f15f35d406067090282fb1a3ee16e640270
Alia <145607497+Aliaokily@users.noreply.github.com>
2024-06-02 05:02:33 +0300

    Asas Elkarama V.1.0

---
+++ js/projects.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('.project-list').isotope({
+        itemSelector: '.item-1',
+        layoutMode: 'fitRows'
+    });
+    })
+
+    $('.project-filter ul li').click(function () {
+    //ACTIVE CLASS
+    $('.project-filter ul li').removeClass('active');
+    $(this).addClass('active');
+
+    var selector = $(this).attr('data-filter');
+    $('.project-list').isotope({
+    filter: selector
+    });
+    return false;
+})
